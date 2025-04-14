65d1e4d6aa1340a0908fac95e68e87dd43a4776d
Lucas Keller <me@lucaskeller.dev>
2025-04-14 14:49:06 -0300

    new configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
+import { federation } from '@module-federation/vite'
+import basicSsl from "@vitejs/plugin-basic-ssl";
+// import fs from 'fs'
 
-// https://vite.dev/config/
+
 export default defineConfig({
+  preview: {
+    host: 'localhost',
+    port: 4173,
+    cors: true
+  },
+  server: {
+    host: 'localhost',
+    port: 4173,
+    cors: true
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        format: 'system',
+        entryFileNames: `[name].js`
+      }
+    }
+  },
   plugins: [
     federation({
-      name: 'mfe1',
+      name: "dummy",
+      filename: 'remoteEntry.js',
       exposes: {
-        './button': './src/components/button',
-      }
+        "./button": "./src/components/button",
+      },
     }),
-    react()
+    react(),
+    basicSsl(),
   ],
-})
+});
